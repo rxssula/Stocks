7c5cebd9c061bb1c0141e73ca12acbad1fdf5e88
Rassul Aitkali <workdir@MacBook-Air-Rassul.local>
2024-03-21 14:54:22 +0500

    Create UITabbarController with StocksNC and FavoritesNC

---
+++ Stocks/FavoritesVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesVC.swift
+//  Stocks
+//
+//  Created by Rassul Aitkali on 21.03.2024.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ Stocks/StocksVC.swift
@@ -0,0 +1,16 @@
+//
+//  StocksVC.swift
+//  Stocks
+//
+//  Created by Rassul Aitkali on 21.03.2024.
+//
+
+import UIKit
+
+class StocksVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
