9d3d200de0d66727f1a4eeee5bb37a9ff4ffd53c
Rassul Aitkali <workdir@MacBook-Air-Rassul.local>
2024-03-31 00:50:32 +0500

    Complete SearchTextField Design with horizontal Stack and add new Font

--- Stocks/StocksVC.swift
+++ Stocks/StocksVC.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class StocksVC: UIViewController {
     
-    let searchTextField = SearchTextField()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -18,13 +16,32 @@ class StocksVC: UIViewController {
     }
     
     func configureSearchTextField() {
-        view.addSubview(searchTextField)
+        let hStack = UIStackView()
+        hStack.axis = .horizontal
+        hStack.spacing = 5
+        hStack.distribution = .fillProportionally
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+        hStack.layer.cornerRadius = 25
+        hStack.layer.borderWidth = 2
+        hStack.layer.borderColor = UIColor.black.cgColor
+        
+        
+        view.addSubview(hStack)
+        
+        let button = UIButton()
+        button.setImage(UIImage(named: "Search") , for: .normal)
+        
+        let searchTextField = UITextField()
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "Find company or ticker", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black, .font: UIFont(name: "MontserratRoman-SemiBold", size: 16)!])
+        
+        hStack.addArrangedSubview(button)
+        hStack.addArrangedSubview(searchTextField)
         
         NSLayoutConstraint.activate([
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            searchTextField.heightAnchor.constraint(equalToConstant: 50)
+            hStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            hStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            hStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            hStack.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
